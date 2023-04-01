a1177f326196f2c87d61f9391acbcb4740771dc8
Parsa Poorsistani <poorsistani13@gmail.com>
2023-04-01 06:25:33 +0330

    FCFS algorithm implemented

--- HW1/algorithms/algorithms.go
+++ HW1/algorithms/algorithms.go
@@ -3,9 +3,26 @@ package algorithms
 import (
 	"OS-course/HW1/structs"
 	"sort"
+	"sync"
 )
 
-func FCFS(processes []structs.Process) {
+func FCFS(processes []*structs.Process) {
 	//sorting based on AT
 	sort.Sort(structs.AtSorter(processes))
+	completionTime := 0
+	var wg sync.WaitGroup
+	for _, process := range processes {
+		wg.Add(1)
+		go func(p *structs.Process) {
+			defer wg.Done()
+			if completionTime < p.AT {
+				completionTime = p.AT
+			}
+			p.CompletionTime = completionTime + p.BT
+			p.TurnaroundTime = float32(p.CompletionTime - p.AT)
+			p.WaitingTime = p.TurnaroundTime - float32(p.BT)
+			completionTime = p.CompletionTime
+		}(process)
+	}
+	wg.Wait()
 }
