f9e971ae5dea81da7ee6fcae5d4aec60bc93cd2e
Parsa Poorsistani <poorsistani13@gmail.com>
2023-04-02 03:37:18 +0330

    implemented rr algo

--- HW1/algorithms/algorithms.go
+++ HW1/algorithms/algorithms.go
@@ -26,3 +26,76 @@ func FCFS(processes []*structs.Process) {
 	}
 	wg.Wait()
 }
+
+func RR(processes []*structs.Process) {
+	totalTime := 0
+	// time := 0
+	// n := len(processes) - 1
+	// queue := make([]structs.Process, n)
+	// remainingProcesses := len(processes)
+	timeQuantum := 2
+	// sort.Sort(structs.AtSorter(processes))
+	for i := 0; i < len(processes); i++ {
+		processes[i].RemainingTime = processes[i].BT
+		totalTime += processes[i].BT
+		// queue = append(queue, *processes[i])
+	}
+	// without goroutine
+	// for len(queue) > 0 {
+	// 	topProcess := queue[0]
+	// 	if topProcess.RemainingTime > 0 {
+	// 		topProcess.RemainingTime -= timeQuantum
+	// 		time += timeQuantum
+	// 		if topProcess.RemainingTime <= 0 {
+	// 			queue = queue[1:]
+	// 			n--
+	// 			topProcess.CompletionTime = time
+	// 			topProcess.TurnaroundTime = float32(time - topProcess.AT)
+	// 			topProcess.WaitingTime = float32(topProcess.TurnaroundTime) - float32(topProcess.BT)
+	// 		} else {
+	// 			for i := n; i < 0; i-- {
+	// 				queue[i-1] = queue[i]
+	// 			}
+	// 			queue[n] = topProcess
+	// 		}
+	// 	}
+	// }
+
+	queue := make(chan *structs.Process)
+	done := make(chan bool)
+	go func() {
+		for _, process := range processes {
+			queue <- process
+		}
+		done <- true
+	}()
+	go func() {
+		time := 0
+		activeProcesses := &structs.Process{}
+		for {
+			if activeProcesses.RemainingTime > 0 {
+				activeProcesses.RemainingTime -= timeQuantum
+				time += timeQuantum
+				if activeProcesses.RemainingTime <= 0 {
+					activeProcesses.CompletionTime = time
+					activeProcesses.TurnaroundTime = float32(time - activeProcesses.AT)
+					activeProcesses.WaitingTime = float32(activeProcesses.TurnaroundTime) - float32(activeProcesses.BT)
+					queue <- activeProcesses
+				}
+			}
+			if activeProcesses.RemainingTime <= 0 {
+				if len(queue) > 0 {
+					activeProcesses = <-queue
+				} else if time >= totalTime {
+					done <- true
+					return
+				} else {
+					time++
+				}
+			}
+		}
+	}()
+	for i := 0; i < len(processes); i++ {
+		<-queue
+	}
+}
